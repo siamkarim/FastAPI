83c10d9ad71d6dcb0aa61504b7310fe547e495c8
siamkarim <siamhossain518@gmail.com>
2023-08-06 22:20:37 +0600

    skip book

--- books.py
+++ books.py
@@ -1,23 +1,26 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
 BOOKS ={
-    'book_1':{'title':'title one','author':'author one'},
-    'book_2':{'title':'title two','author':'author two'},
-    'book_3':{'title':'title three','author':'author three'},
-    'book_4':{'title':'title four','author':'author four'},
-    'book_5':{'title':'title five','author':'author five'},
+    'book_1': {'title':'title one','author':'author one'},
+    'book_2': {'title':'title two','author':'author two'},
+    'book_3': {'title':'title three','author':'author three'},
+    'book_4': {'title':'title four','author':'author four'},
+    'book_5': {'title':'title five','author':'author five'},
 }
 
+
+
 @app.get("/")
-async def read_all_books():
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
     return BOOKS
 
-@app.get("/books/mybooks")
-async def my_favourite_books():
-    return {"book_title": "my favourite books"}
 
-@app.get("/books/{book_id}")
-async def read_books(book_id: int):
-    return {"book_title": book_id}
+@app.get("/{book_name}")
+async def read_books(book_name: str ):
+    return BOOKS[book_name]
